1675f83ecb1e7bc0ac66bbb3492b328fe0e9163e
TheDanishWonder <cph-nd76@cphbusiness.dk>
2020-10-16 22:43:58 +0200

    mismatch

--- App.js
+++ App.js
@@ -1,43 +1,61 @@
-import React from 'react'
-import { UIManager } from 'react-native'
-import { Provider } from 'unstated'
-import { Provider as PaperProvider } from 'react-native-paper'
+import React from "react";
+import { UIManager } from "react-native";
+import { Provider } from "unstated";
+import { Provider as PaperProvider } from "react-native-paper";
+import Navigation from "./src/Navigation";
 import { AppLoading } from 'expo'
 import * as Font from 'expo-font'
 
-import Navigation from './src/Navigation'
+import theme from "./src/config/theme";
+import * as SplashScreen from 'expo-splash-screen';
 
-import theme from './src/config/theme'
+UIManager.setLayoutAnimationEnabledExperimental &&
+  UIManager.setLayoutAnimationEnabledExperimental(true);
 
-UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true)
+// Prevent native splash screen from autohiding before App component declaration
+SplashScreen.preventAutoHideAsync()
+  .then(result => console.log(`SplashScreen.preventAutoHideAsync() succeeded: ${result}`))
+  .catch(console.warn); // it's good to explicitly catch and inspect any error
 
 export default class App extends React.Component {
   state = {
-    fontLoaded: false
-  }
+    fontLoaded: false,
+  };
 
-  async componentDidMount () {
-    await Font.loadAsync({
-      'GloriaHallelujah': require('./src/fonts/GloriaHallelujah.ttf')
-    })
-
-    this.setState({ fontLoaded: true })
-  }
-
-  render () {
-    const { fontLoaded } = this.state
-
-    if (!fontLoaded) {
-      return <AppLoading />
+  async componentDidMount() {
+    // Prevent native splash screen from autohiding
+    try {
+      await SplashScreen.preventAutoHideAsync();
+    } catch (e) {
+      console.warn(e);
     }
+    this.prepareResources();
+    await Font.loadAsync({
+      'GloriaHallelujah': require('./src/fonts/GloriaHallelujah.ttf'),
+    })
+    this.setState({ loading: true})
+  }
 
+  prepareResources = async () => {
+    this.setState({ appIsReady: true }, async () => {
+      await SplashScreen.hideAsync();
+    });
+  }
+
+  render() {
+    if (!this.state.appIsReady) {
+      return null;
+    }
+    
+    if (!this.state.loading) {
+      return null;
+    }
     return (
       <PaperProvider theme={theme}>
         <Provider>
           <Navigation />
         </Provider>
       </PaperProvider>
-
-    )
+    );
   }
 }
